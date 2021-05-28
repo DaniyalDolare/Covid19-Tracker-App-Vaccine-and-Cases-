9a4cd08eeec6f5537bee7d4ba8dc4f6f2ab699f5
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-28 22:04:09 +0530

    global data card added

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -2,6 +2,11 @@ import 'package:covid19_tracker/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> globalDataList = [
+    {"title": "Total Confirmed", "number": 169879799, "color": pColor},
+    {"title": "Total Deaths", "number": 3530136, "color": Colors.red},
+    {"title": "Total Recovered", "number": 151786249, "color": Colors.black}
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +39,55 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               Column(
-                children: [],
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 16,
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(10.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    elevation: 4.0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "Globally",
+                              style: TextStyle(
+                                  fontSize: 16.0, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          for (var item in globalDataList)
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    item["title"],
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey[600]),
+                                  ),
+                                  Text(
+                                    item["number"].toString(),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: item["color"]),
+                                  )
+                                ],
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               )
             ],
           ),
