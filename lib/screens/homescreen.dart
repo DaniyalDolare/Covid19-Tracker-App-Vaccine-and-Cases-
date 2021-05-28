a710c695dea56f281ad2ef7dc3ed8dba4c39aa56
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-28 20:14:21 +0530

    top bg container added

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,8 +1,41 @@
+import 'package:covid19_tracker/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 8,
+                width: double.infinity,
+                color: pColor,
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 15.0, bottom: 10.0, left: 10.0),
+                    child: Text(
+                      "Dashboard",
+                      style: TextStyle(
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[50]),
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: [],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
