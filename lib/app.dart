f8a28a4368514d5ac52d373315000c0750916353
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-28 14:22:30 +0530

    extracted app and homescreen from main function

---
+++ lib/app.dart
@@ -0,0 +1,9 @@
+import 'package:covid19_tracker/screens/homescreen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}
