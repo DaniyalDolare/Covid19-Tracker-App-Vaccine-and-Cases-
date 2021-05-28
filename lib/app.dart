22eb6a6956d4c855e96603ee19fbd07e9efbafd9
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-28 20:05:55 +0530

    theme data added in app

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,15 @@
 import 'package:covid19_tracker/screens/homescreen.dart';
+import 'package:covid19_tracker/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return MaterialApp(
+        theme: ThemeData(
+          primaryColor: pColor,
+          primarySwatch: pSwatch,
+        ),
+        home: HomeScreen());
   }
 }
