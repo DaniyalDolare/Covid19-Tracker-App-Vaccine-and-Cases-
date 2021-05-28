d288507423bc30cd9b5f39cf1699ff419a6486e7
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-28 21:10:36 +0530

    swatch color updated

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,16 +4,16 @@ const pColor = Color(0xFF61C4A9);
 const bgColor = Color(0xFFF5F5F5);
 
 const color = const {
-  50: Color.fromRGBO(136, 14, 79, .1),
-  100: Color.fromRGBO(136, 14, 79, .2),
-  200: Color.fromRGBO(136, 14, 79, .3),
-  300: Color.fromRGBO(136, 14, 79, .4),
-  400: Color.fromRGBO(136, 14, 79, .5),
-  500: Color.fromRGBO(136, 14, 79, .6),
-  600: Color.fromRGBO(136, 14, 79, .7),
-  700: Color.fromRGBO(136, 14, 79, .8),
-  800: Color.fromRGBO(136, 14, 79, .9),
-  900: Color.fromRGBO(136, 14, 79, 1),
+  50: Color.fromRGBO(97, 196, 169, .1),
+  100: Color.fromRGBO(97, 196, 169, .2),
+  200: Color.fromRGBO(97, 196, 169, .3),
+  300: Color.fromRGBO(97, 196, 169, .4),
+  400: Color.fromRGBO(97, 196, 169, .5),
+  500: Color.fromRGBO(97, 196, 169, .6),
+  600: Color.fromRGBO(97, 196, 169, .7),
+  700: Color.fromRGBO(97, 196, 169, .8),
+  800: Color.fromRGBO(97, 196, 169, .9),
+  900: Color.fromRGBO(97, 196, 169, 1),
 };
 
 const pSwatch = const MaterialColor(0xFF61C4A9, color);
