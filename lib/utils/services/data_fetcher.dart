15abb90e252e48ac3bb0efb5ceb362a4e34f4a1c
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-05-31 11:42:29 +0530

    exception handling added

--- lib/utils/services/data_fetcher.dart
+++ lib/utils/services/data_fetcher.dart
@@ -1,18 +1,28 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:covid19_tracker/utils/custom_exception.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import '../constants.dart';
 
+final headers = {
+  "x-rapidapi-host": "covid-193.p.rapidapi.com",
+  "x-rapidapi-key": "f7de9c9031mshc6181de5aca69b3p1f88a8jsn8140b4cacc69",
+};
+
 Future<Map<String, dynamic>> fetchStatistics() async {
   // Get statistics data from api
+
   final url = Uri.https("covid-193.p.rapidapi.com", "statistics");
-  final headers = {
-    "x-rapidapi-host": "covid-193.p.rapidapi.com",
-    "x-rapidapi-key": "f7de9c9031mshc6181de5aca69b3p1f88a8jsn8140b4cacc69",
-  };
-  final response = await http.get(url, headers: headers);
-  final responseBody = jsonDecode(response.body);
+  var responseBody;
+
+  try {
+    final response = await http.get(url, headers: headers);
+    responseBody = _response(response);
+  } on SocketException {
+    throw FetchDataException("No internet connection");
+  }
   final responseList = responseBody["response"] as List<dynamic>;
 
   // Get saved country list from pref else use default
@@ -23,8 +33,7 @@ Future<Map<String, dynamic>> fetchStatistics() async {
   if (!areSaved) {
     pref.setStringList("savedCountries", ["USA", "India", "Brazil", "France"]);
   }
-  List<String>? savedCountryList = pref.getStringList("savedCountries") ??
-      ["USA", "India", "Brazil", "France"];
+  List<String>? savedCountryList = pref.getStringList("savedCountries");
 
   // Process data into required format
   List<Map<String, dynamic>> globalDataList = [
@@ -32,7 +41,7 @@ Future<Map<String, dynamic>> fetchStatistics() async {
     {"title": "Total Deaths", "number": "", "color": Colors.red},
     {"title": "Total Recovered", "number": "", "color": Colors.black}
   ];
-  List<Map<String, dynamic>> countryDataList = [];
+  List<Map<String, dynamic>> allCountryDataList = [];
   for (Map<String, dynamic> response in responseList) {
     if (response["country"] == "All") {
       // Set the global data
@@ -40,20 +49,17 @@ Future<Map<String, dynamic>> fetchStatistics() async {
       globalDataList[1]["number"] = response["deaths"]["total"] ?? 0;
       globalDataList[2]["number"] = response["cases"]["recovered"] ?? 0;
     } else {
-      for (String country in savedCountryList) {
-        // For countries in savedCountries, add data to country list
-        if (response["country"] == country) {
-          countryDataList.add({
-            "name": response["country"],
-            "total": response["cases"]["total"] ?? 0
-          });
-        }
-      }
+      allCountryDataList.add({
+        "name": response["country"],
+        "total": response["cases"]["total"] ?? 0
+      });
     }
   }
+  // Make a map from global,saved and allcountrydata list
   Map<String, dynamic> data = {
     "globalDataList": globalDataList,
-    "countryDataList": countryDataList
+    "allCountryDataList": allCountryDataList,
+    "savedCountryList": savedCountryList!.toSet()
   };
 
   // Return the data
@@ -62,13 +68,17 @@ Future<Map<String, dynamic>> fetchStatistics() async {
 
 Future<List<Map<String, dynamic>>> getCountryList() async {
   // Get country list from api
+
   final url = Uri.https("covid-193.p.rapidapi.com", "countries");
-  final headers = {
-    "x-rapidapi-host": "covid-193.p.rapidapi.com",
-    "x-rapidapi-key": "f7de9c9031mshc6181de5aca69b3p1f88a8jsn8140b4cacc69",
-  };
-  final response = await http.get(url, headers: headers);
-  final responseBody = jsonDecode(response.body);
+  var responseBody;
+
+  try {
+    final response = await http.get(url, headers: headers);
+    responseBody = _response(response);
+  } on SocketException {
+    throw FetchDataException("No internet connection");
+  }
+
   final countries = responseBody["response"] as List<dynamic>;
 
   // Get saved country list from pref
@@ -85,4 +95,21 @@ Future<List<Map<String, dynamic>>> getCountryList() async {
     }
   }
   return countryList;
+}
+
+dynamic _response(http.Response response) {
+  switch (response.statusCode) {
+    case 200:
+      var responseJson = json.decode(response.body.toString());
+      return responseJson;
+    case 400:
+      throw BadRequestException(response.body.toString());
+    case 401:
+    case 403:
+      throw UnauthorisedException(response.body.toString());
+    case 500:
+    default:
+      throw FetchDataException(
+          'Error occured while Communication with Server with StatusCode: ${response.statusCode}');
+  }
 }
